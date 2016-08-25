a4523b8cd5210b25f49525ef614d22b6f7fe4aa2
icole <ian.cole@me.com>
2016-08-25 00:46:00 -0700

    Stub out part 2

--- week3/objsets/src/main/scala/objsets/TweetSet.scala
+++ week3/objsets/src/main/scala/objsets/TweetSet.scala
@@ -56,7 +56,7 @@ abstract class TweetSet {
     * Question: Should we implment this method here, or should it remain abstract
     * and be implemented in the subclasses?
     */
-  def union(that: TweetSet): TweetSet = ???
+  def union(that: TweetSet): TweetSet
 
   /**
     * Returns the tweet from this set which has the greatest retweet count.
@@ -111,6 +111,8 @@ abstract class TweetSet {
 class Empty extends TweetSet {
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
 
+  def union(that: TweetSet): TweetSet = that
+
   /**
     * The following methods are already implemented
     */
@@ -130,6 +132,7 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     if (p(elem)) right.filterAcc(p, left.filterAcc(p, acc)).incl(elem)
     else right.filterAcc(p, left.filterAcc(p, acc))
 
+  def union(that: TweetSet): TweetSet = this
 
   /**
     * The following methods are already implemented
